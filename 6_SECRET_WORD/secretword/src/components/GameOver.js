7209eeefd232ebeae27bf3d3e0f91be29e50770e
Ramonlirani <ramonlirani@hotmail.com>
2022-07-08 16:05:21 -0300

    jogo finalizado

--- 6_SECRET_WORD/secretword/src/components/GameOver.js
+++ 6_SECRET_WORD/secretword/src/components/GameOver.js
@@ -1,8 +1,16 @@
 import './GameOver.css';
 
-const GameOver = () => {
+const GameOver = ({retry, score}) => {
   return (
-    <div>GameOver</div>
+    <div>
+      <h1>
+        Fim de jogo
+      </h1>
+      <h2>
+        A sua pontuação foi: <span>{score}</span>
+        </h2>
+      <button onClick={retry}>Resetar jogo</button>
+    </div>
   )
 }
 
