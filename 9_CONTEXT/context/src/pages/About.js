ba0cb0036981bc01880221222e73f94b7fe7bb96
Ramonlirani <ramonlirani@hotmail.com>
2022-07-20 15:50:50 -0300

    context finalizado

--- 9_CONTEXT/context/src/pages/About.js
+++ 9_CONTEXT/context/src/pages/About.js
@@ -1,7 +1,21 @@
-const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+import { useContext } from "react";
 
-export default About
+import { CounterContext } from "../context/CounterContext";
+
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
+
+const About = () => {
+  const { counter } = useContext(CounterContext);
+
+  // 5 - contexto mais complexo
+  const { color } = useTitleColorContext();
+
+  return (
+    <div>
+      <h1 style={{ color: color }}>About</h1>
+      <p>Valor contador: {counter}</p>
+    </div>
+  );
+};
+
+export default About;
--- 9_CONTEXT/context/src/pages/Products.js
+++ 9_CONTEXT/context/src/pages/Products.js
@@ -1,6 +1,16 @@
+//import { useContext } from "react";
+//import { CounterContext } from "../context/CounterContext";
+import { useCounterContext } from "../hooks/useCounterContext";
+
 const Products = () => {
+  //const { counter } = useContext(CounterContext);
+  const { counter } = useCounterContext();
+
   return (
-    <div>Products</div>
+    <div>
+    <h1>Products</h1>
+    <p>Valor do contador: {counter}</p>
+  </div>
   )
 }
 
