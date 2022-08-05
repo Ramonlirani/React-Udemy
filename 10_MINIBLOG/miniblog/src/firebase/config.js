29b2ffa6e81ac04be462d729aa4124c9bc07def2
Ramonlirani <ramonlirani@hotmail.com>
2022-08-05 16:13:35 -0300

    registro de usuario no sistema

--- 10_MINIBLOG/miniblog/src/firebase/config.js
+++ 10_MINIBLOG/miniblog/src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firebase";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAundu8n1xgBsrEYzwPE06fQU5omrryCLs",
