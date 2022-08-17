e39192388329bda4ee81b76cb8f00a0d44ec7dcf
Ramonlirani <ramonlirani@hotmail.com>
2022-08-17 00:07:30 -0300

    useReducer

--- 11_HOOKS/reacthooks/src/pages/Home.js
+++ 11_HOOKS/reacthooks/src/pages/Home.js
@@ -1,4 +1,4 @@
-import  {HookUseReducer}  from "../components/HookUseReducer"
+import HookUseReducer  from "../components/HookUseReducer"
 import HookUseState from "../components/HookUseState"
 
 const Home = () => {
